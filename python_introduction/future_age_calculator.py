532d7c1b5c7b268a77ed1d0e3837d027ec32d00c
Freddy Pastaff <amoakof92@gmail.com>
2025-09-10 21:13:44 +0000

    future age calculator script

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+current_age = input ("How old are you? ")
+current_year = 2023
+future_year = 2050
+# subract 2050 from 2023 which gives you 27.
+#thus:
+age = 27 + int (current_age)
+print (f"In 2050, you will be {age} years old")
+
