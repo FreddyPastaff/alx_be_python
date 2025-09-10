98e1c4eda745eee36e50d26bc290581e969f4e6a
Freddy Pastaff <amoakof92@gmail.com>
2025-09-10 15:49:08 +0000

    python_introduction

--- basic_operations.py
+++ basic_operations.py
@@ -1,5 +1,14 @@
 number1 = 10
 number2 = 5
-print (number1+number2)
-print (number1-number2)
-print (number1*number2)
+addition = number1 + number2
+print ("addition of 10 and 5 is ", addition)
+
+number1 = 10
+number2 = 5
+subtraction = number1 - number2
+print ("subtraction of 10 and 5 is ", subtraction)
+
+number1 = 10 
+number2 = 5
+Multiplication = 10*5
+print ("Multiplication of 10 and 5 is ", Multiplication)
