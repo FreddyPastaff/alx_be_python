a23ba8019ec3d87be697b49e38e6b128eb9a6301
Freddy Pastaff <amoakof92@gmail.com>
2025-09-29 22:04:36 +0000

    shopping_list_manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,40 @@
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Enter the item to add")
+            shopping_list.append(item)
+            print(f"Item added is: {item}")
+
+        elif choice == '2':
+            item = input("Enter the item to remove.")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} was removed from the list.")
+            else:
+                print(f"{item} was not found in the list.")
+
+        elif choice == '3':
+            print("Shopping List:")
+            for item in shopping_list:
+                print(f"- {item}")
+
+        elif choice == '4':
+            print("Exiting Shopping List Manager.")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+    
