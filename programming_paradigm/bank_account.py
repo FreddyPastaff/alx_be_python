0f32e5651bd207e872b9d1b5f43291a803e06a88
Freddy Pastaff <amoakof92@gmail.com>
2025-10-05 15:30:09 +0000

    bank_account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0.0):
+        self.account_balance = initial_balance
+
+# Desposit Method
+def deposit(self, amount):
+    self.account_balance += amount
+
+# Withdraw Method
+def withdraw(self, amount):
+    if amount > self.account_balance:
+        return "Insufficient fund"
+    else:
+        self.account_balance -= amount
+        return f"New balance: ${self.account_balance:.2f}"
+
+# Balance Inquiry: 
+def check_account_balance(self):
+    return f"current balance: ${self.account_balance:.2f}"
+
